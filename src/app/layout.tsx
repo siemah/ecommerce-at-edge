f5afa925698c7d50b5b2182333b91d2830f3dca3
siemah <hmi.dayenio@gmail.com>
2023-07-21 18:52:46 +0100

    ref:move some parts 2separate components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./globals.css"
 import "@/assets/css/main.css";
 import { Roboto, Poppins } from "next/font/google";
 import Logo from "@/assets/images/logo.svg";
 import { Metadata } from "next";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -39,200 +40,15 @@ export default function RootLayout({
       </head>
       <body className={`${poppins.className} ${roboto.className}`}>
 
-        {/* <!-- header --> */}
-        <header className="py-4 shadow-sm bg-white">
-          <div className="container flex items-center justify-between">
-            <a href="index.html">
-              <Image
-                src={Logo}
-                alt="Logo"
-                className="w-32"
-              />
-            </a>
-
-            <div className="w-full max-w-xl relative flex">
-              <span className="absolute left-4 top-3 text-lg text-gray-400">
-                <i className="fa-solid fa-magnifying-glass"></i>
-              </span>
-              <input type="text" name="search" id="search"
-                className="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
-                placeholder="search" />
-              <button className="bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex justify-center items-center">
-                Search
-              </button>
-            </div>
-
-            <div className="flex items-center space-x-4">
-              <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
-                <div className="text-2xl">
-                  <i className="fa-regular fa-heart"></i>
-                </div>
-                <div className="text-xs leading-3">Wishlist</div>
-                <div
-                  className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-                  8</div>
-              </a>
-              <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
-                <div className="text-2xl">
-                  <i className="fa-solid fa-bag-shopping"></i>
-                </div>
-                <div className="text-xs leading-3">Cart</div>
-                <div
-                  className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-                  2</div>
-              </a>
-              <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
-                <div className="text-2xl">
-                  <i className="fa-regular fa-user"></i>
-                </div>
-                <div className="text-xs leading-3">Account</div>
-              </a>
-            </div>
-          </div>
-        </header>
-        {/* <!-- ./header --> */}
-
-        {/* <!-- navbar --> */}
-        <nav className="bg-gray-800">
-          <div className="container flex">
-            <div className="px-8 py-4 bg-primary md:flex items-center cursor-pointer relative group hidden">
-              <span className="text-white">
-                <i className="fa-solid fa-bars"></i>
-              </span>
-              <span className="capitalize ml-2 text-white hidden">All Categories</span>
-
-              {/* <!-- dropdown --> */}
-              <div
-                className="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
-                <a href="#" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-                  <img src="https://picsum.photos/1006" alt="sofa" className="w-5 h-5 object-contain" />
-                  <span className="ml-6 text-gray-600 text-sm">Sofa</span>
-                </a>
-                <a href="#" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-                  <img src="https://picsum.photos/1000" alt="terrace" className="w-5 h-5 object-contain" />
-                  <span className="ml-6 text-gray-600 text-sm">Terarce</span>
-                </a>
-                <a href="#" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-                  <img src="https://picsum.photos/1004" alt="bed" className="w-5 h-5 object-contain" />
-                  <span className="ml-6 text-gray-600 text-sm">Bed</span>
-                </a>
-                <a href="#" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-                  <img src="https://picsum.photos/1003" alt="office" className="w-5 h-5 object-contain" />
-                  <span className="ml-6 text-gray-600 text-sm">office</span>
-                </a>
-                <a href="#" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-                  <img src="https://picsum.photos/1002" alt="outdoor" className="w-5 h-5 object-contain" />
-                  <span className="ml-6 text-gray-600 text-sm">Outdoor</span>
-                </a>
-                <a href="#" className="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-                  <img src="https://picsum.photos/1001" alt="Mattress" className="w-5 h-5 object-contain" />
-                  <span className="ml-6 text-gray-600 text-sm">Mattress</span>
-                </a>
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between flex-grow md:pl-12 py-5">
-              <div className="flex items-center space-x-6 capitalize">
-                <a href="index.html" className="text-gray-200 hover:text-white transition">Home</a>
-                <a href="pages/shop.html" className="text-gray-200 hover:text-white transition">Shop</a>
-                <a href="#" className="text-gray-200 hover:text-white transition">About us</a>
-                <a href="#" className="text-gray-200 hover:text-white transition">Contact us</a>
-              </div>
-              <a href="pages/login.html" className="text-gray-200 hover:text-white transition">Login</a>
-            </div>
-          </div>
-        </nav>
-        {/* <!-- ./navbar --> */}
-
+        {/* header */}
+        <Header logo={Logo} />
+        {/* main ui */}
         <main>
           {children}
         </main>
+        {/* footer */}
+        <Footer logo={Logo} />
 
-        {/* <!-- footer --> */}
-        <footer className="bg-white pt-16 pb-12 border-t border-gray-100">
-          <div className="container grid grid-cols-1 ">
-            <div className="col-span-1 space-y-4">
-              <Image
-                src={Logo}
-                alt="logo"
-                className="w-30"
-              />
-              <div className="mr-2">
-                <p className="text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, hic?
-                </p>
-              </div>
-              <div className="flex space-x-5">
-                <a href="#" className="text-gray-400 hover:text-gray-500"><i
-                  className="fa-brands fa-facebook-square"></i></a>
-                <a href="#" className="text-gray-400 hover:text-gray-500"><i
-                  className="fa-brands fa-instagram-square"></i></a>
-                <a href="#" className="text-gray-400 hover:text-gray-500"><i
-                  className="fa-brands fa-twitter-square"></i></a>
-                <a href="#" className="text-gray-400 hover:text-gray-500">
-                  <i className="fa-brands fa-github-square"></i>
-                </a>
-              </div>
-            </div>
-
-            <div className="col-span-2 grid grid-cols-2 gap-4">
-              <div className="grid grid-cols-2 gap-4 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
-                  <div className="mt-4 space-y-4">
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
-                  </div>
-                </div>
-
-                <div>
-                  <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
-                  <div className="mt-4 space-y-4">
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
-                    {/* <!-- <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Documentation</a> --> */}
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
-                  </div>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 gap-8">
-                <div>
-                  <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
-                  <div className="mt-4 space-y-4">
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
-                  </div>
-                </div>
-
-                <div>
-                  <h3 className="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
-                  <div className="mt-4 space-y-4">
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
-                    {/* <!-- <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Documentation</a> --> */}
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-        {/* <!-- ./footer --> */}
-
-        {/* <!-- copyright --> */}
-        <div className="bg-gray-800 py-4">
-          <div className="container flex items-center justify-between">
-            <p className="text-white">&copy; TailCommerce - All Right Reserved</p>
-            <div>
-              <img src="https://picsum.photos/1011" alt="methods" className="h-5" />
-            </div>
-          </div>
-        </div>
-        {/* <!-- ./copyright --> */}
       </body>
     </html>
   )
---
+++ src/components/header/index.tsx
@@ -0,0 +1,167 @@
+import Image from 'next/image';
+import React from 'react'
+
+export default function Header({ logo }: { logo: string }) {
+  return (
+    <div>
+      {/* <!-- header --> */}
+      <header className="py-4 shadow-sm bg-white">
+        <div className="container flex items-center justify-between">
+          <a href="index.html">
+            <Image
+              src={logo}
+              alt="Logo"
+              className="w-32"
+            />
+          </a>
+
+          <div className="w-full max-w-xl relative flex">
+            <span className="absolute left-4 top-3 text-lg text-gray-400">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </span>
+            <input type="text" name="search" id="search"
+              className="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
+              placeholder="search" />
+            <button className="bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex justify-center items-center">
+              Search
+            </button>
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
+              <div className="text-2xl">
+                <i className="fa-regular fa-heart"></i>
+              </div>
+              <div className="text-xs leading-3">Wishlist</div>
+              <div
+                className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
+                8</div>
+            </a>
+            <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
+              <div className="text-2xl">
+                <i className="fa-solid fa-bag-shopping"></i>
+              </div>
+              <div className="text-xs leading-3">Cart</div>
+              <div
+                className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
+                2</div>
+            </a>
+            <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
+              <div className="text-2xl">
+                <i className="fa-regular fa-user"></i>
+              </div>
+              <div className="text-xs leading-3">Account</div>
+            </a>
+          </div>
+        </div>
+      </header>
+      {/* <!-- ./header --> */}
+
+      {/* <!-- navbar --> */}
+      <nav className="bg-gray-800">
+        <div className="container flex">
+          <div className="px-8 py-4 bg-primary md:flex items-center cursor-pointer relative group hidden">
+            <span className="text-white">
+              <i className="fa-solid fa-bars"></i>
+            </span>
+            <span className="capitalize ml-2 text-white hidden">All Categories</span>
+
+            {/* <!-- dropdown --> */}
+            <div
+              className="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
+              <a href="#" className="flex gap-2 items-center px-6 py-3 hover:bg-gray-100 transition w-max h-5">
+                <div className="relative w-5 h-5">
+                  <Image
+                    src="https://picsum.photos/1006"
+                    fill
+                    loading='lazy'
+                    unoptimized
+                    alt="sofa"
+                    className="w-5 object-contain"
+                  />
+                </div>
+                <span className="flex-1 text-gray-600 text-sm">Sofa</span>
+              </a>
+              <a href="#" className="flex gap-2 items-center px-6 py-3 hover:bg-gray-100 transition w-max h-5">
+                <div className="relative w-5 h-5">
+                  <Image
+                    src="https://picsum.photos/1000"
+                    fill
+                    loading='lazy'
+                    unoptimized
+                    alt="terrace"
+                    className="w-5 object-contain"
+                  />
+                </div>
+                <span className="flex-1 text-gray-600 text-sm">Terarce</span>
+              </a>
+              <a href="#" className="flex gap-2 items-center px-6 py-3 hover:bg-gray-100 transition w-max h-5">
+                <div className="relative w-5 h-5">
+                  <Image
+                    src="https://picsum.photos/1004"
+                    fill
+                    loading='lazy'
+                    unoptimized
+                    alt="bed"
+                    className="w-5 object-contain"
+                  />
+                </div>
+                <span className="flex-1 text-gray-600 text-sm">Bed</span>
+              </a>
+              <a href="#" className="flex gap-2 items-center px-6 py-3 hover:bg-gray-100 transition w-max h-5">
+                <div className="relative w-5 h-5">
+                  <Image
+                    src="https://picsum.photos/1003"
+                    fill
+                    loading='lazy'
+                    unoptimized
+                    alt="office"
+                    className="w-5 object-contain"
+                  />
+                </div>
+                <span className="flex-1 text-gray-600 text-sm">office</span>
+              </a>
+              <a href="#" className="flex gap-2 items-center px-6 py-3 hover:bg-gray-100 transition w-max h-5">
+                <div className="relative w-5 h-5">
+                  <Image
+                    src="https://picsum.photos/1002"
+                    fill
+                    loading='lazy'
+                    unoptimized
+                    alt="outdoor"
+                    className="w-5 object-contain"
+                  />
+                </div>
+                <span className="flex-1 text-gray-600 text-sm">Outdoor</span>
+              </a>
+              <a href="#" className="flex gap-2 items-center px-6 py-3 hover:bg-gray-100 transition w-max h-5">
+                <div className="relative w-5 h-5">
+                  <Image
+                    src="https://picsum.photos/1001"
+                    fill
+                    loading='lazy'
+                    unoptimized
+                    alt="Mattress"
+                    className="w-5 object-contain"
+                  />
+                </div>
+                <span className="flex-1 text-gray-600 text-sm">Mattress</span>
+              </a>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between flex-grow md:pl-12 py-5">
+            <div className="flex items-center space-x-6 capitalize">
+              <a href="index.html" className="text-gray-200 hover:text-white transition">Home</a>
+              <a href="pages/shop.html" className="text-gray-200 hover:text-white transition">Shop</a>
+              <a href="#" className="text-gray-200 hover:text-white transition">About us</a>
+              <a href="#" className="text-gray-200 hover:text-white transition">Contact us</a>
+            </div>
+            <a href="pages/login.html" className="text-gray-200 hover:text-white transition">Login</a>
+          </div>
+        </div>
+      </nav>
+      {/* <!-- ./navbar --> */}
+    </div>
+  );
+}
