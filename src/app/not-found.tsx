f20b446403cbad789ceb8ab1968e571cf84772e4
siemah <hmi.dayenio@gmail.com>
2023-07-23 16:24:22 +0100

    feat:enhance 404 ui

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,38 @@
+import Footer from '@/components/footer';
+import Header from '@/components/header';
 import Link from 'next/link'
+import Logo from "@/assets/images/logo.svg";
+import { Poppins } from 'next/font/google';
+import { Metadata } from 'next';
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
+export const metadata: Metadata = {
+  title: 'Not Found - 404',
+  description: 'this page doesnt exists',
+  creator: "siemah",
+  alternates: {
+    canonical: "/",
+  },
+};
 
 export default function Page404() {
   return (
-    <main className="flex flex-col gap-6 justify-center items-center bg-purple-500 h-96 w-full">
-      <h1>Not Found - 404</h1>
-      <Link
-        href={"/"}
-        className='px-6 py-3 bg-purple-800 text-white rounded-md outline outline-offset-2 outline-purple-300'
-      >
-        Go home
-      </Link>
-    </main>
+    <body className={`${poppins.className}`}>
+      <Header logo={Logo} />
+      <main className="flex flex-col gap-6 justify-center items-center bg-purple-500 w-full h-screen">
+        <h1>Not Found - 404</h1>
+        <Link
+          href={"/"}
+          className='px-6 py-3 bg-purple-800 text-white rounded-md outline outline-offset-2 outline-purple-300 hover:bg-purple-950 hover:outline-dashed transition-all'
+        >
+          Go home
+        </Link>
+      </main>
+      <Footer logo={Logo} />
+    </body>
   );
 }
