9225906adafb8d1903e2e9e1dcb189ebd41d1abe
siemah <hmi.dayenio@gmail.com>
2023-07-29 18:13:38 +0100

    fix:specify runtime 2edge

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"]
 });
-
+export const runtime = "edge";
 export const metadata: Metadata = {
   title: 'Not Found - 404',
   description: 'this page doesnt exists',
